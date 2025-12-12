bb27d2ca8060ee1b316151ab35b77824091dc623
BjerregaardGG <oliverbjerregaard1@gmail.com>
2025-12-12 13:48:07 +0100

    Problems with hot reload

---
+++ client/src/modules.d.ts
@@ -0,0 +1,19 @@
+
+// Sørger for, at TypeScript anerkender 'socket.io-client' som et modul,
+// der kan importeres, ofte med en default export (io).
+declare module 'socket.io-client' {
+    import * as io from 'socket.io-client/dist/socket.io';
+    export default io;
+}
+
+declare module 'svelte-routing' {
+    export const Link: any;
+    
+    export const Router: any;
+    
+    export const Route: any;
+    
+    export const navigate: any;
+
+    export const route: any;
+}
